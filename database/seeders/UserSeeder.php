59c899db57b5776a0179bd95befc9abbb23efaea
juelz <125363529+9uli9@users.noreply.github.com>
2023-11-27 11:03:54 +0000

    Created User Roles Relation

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\User;
+use App\Models\Role;
 
 class UserSeeder extends Seeder
 {
@@ -13,11 +14,32 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
+
+        $role_admin = Role::where('name', 'admin')->first();
+
+        $role_user = Role::where('name', 'user')->first();
+
+
+
+
         $admin = new User;
-        $admin->name = "Mo Che";
-        $admin->email = "admin@ca1example.com";
+        $admin->name = "Julia Szew";
+        $admin->email = "admin@ca1example2.com";
         $admin->password = "secret123";
         $admin->save();
+
+        //attach admin role to the user created above
+        $admin->roles()->attach($role_admin);
+
+
+        $admin = new User;
+        $admin->name = "Mo Che";
+        $admin->email = "admin@ca1example1.com";
+        $admin->password = "secret123";
+        $admin->save();
+
+        //attach admin role to the user created above
+        $admin->roles()->attach($role_user);
 
     }
 }
